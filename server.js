fd6ab4c5977d5a0d494df6f154d0c259d39bee30
Dhairya Jain <dhairyajainrocks@gmail.com>
2023-04-25 21:46:37 +0530

    connecting backend and database

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const postRoutes = require('./backend/postRoutes')
+const postRoutes = require('./backend/postRoutes')
 
 const port = process.env.PORT || 3000
 
 const app = express()
+
+connectDB()
 
 //middleware which hadles parsing incoming json data from front end
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 //localhost:3000/api/posts
-app.use('/api/posts',postRoutes)
+app.use('/api/posts', postRoutes)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
